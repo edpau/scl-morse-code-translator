3034be9e8dc2332fb99f92e0bdd4ecd0706ca0e4
edpau <edpau@me.com>
2025-07-12 12:24:32 +0100

    feat: add Translator and MorseCode with basic encode flow (no error handling yet)
    - Added Translator.encode for word-by-word + char-by-char mapping
    - Created MorseCode object with initial char-to-Morse map
    - Connected Translator logic in Main.scala
    - Input validation exists, but no fallback for unsupported characters yet

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,17 @@
 object Main {
-  def main (args: Array[String]): Unit = {
-    println("🤖Welcome to Morse Code Translator!\n")
+  def main(args: Array[String]): Unit = {
+    println("🤖 Welcome to Morse Code Translator!\n")
 
     val input = InputHandler.promptUser("Please type what you want to translate: ")
-    
-    println(input)
+    println("input: " + input)
+
+    // TODO: Move input validation to Validation.scala
+    // Includes: check for empty input, unsupported characters and max length
+    if (!input.forall(MorseCode.morseMap.contains))
+      throw new IllegalArgumentException("Input contains invalid characters.")
+
+    val encodedText  = Translator.encode(input)
+    println("Encoded output: " + encodedText )
+
   }
 }
