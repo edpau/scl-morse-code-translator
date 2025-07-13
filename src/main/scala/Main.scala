a9f21a37317d74799bc92d9a88c0095639a73163
edpau <edpau@me.com>
2025-07-13 18:37:21 +0100

    feat: implement basic encode-only main loop with tail-recursive retry
    - Adds a working while loop to encode English to Morse.
    - Includes tail-recursive askToContinue() for clean user retry.
    - Prepares the ground for future menu-based navigation (encode/decode/exit).

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,18 +1,41 @@
+import scala.annotation.tailrec
+
 object Main {
   def main(args: Array[String]): Unit = {
     OutputHandler.printWelcome()
 
-    OutputHandler.printPrompt("Enter your message (English → Morse):")
-    val input = InputHandler.getUserInput()
-    println(s"[Debug] input: $input")
+    var keepTranslating = true
+    while (keepTranslating) {
 
-    // TODO: Move input validation to Validation.scala
-    // Includes: check for empty input, unsupported characters and max length
-    if (!input.forall(MorseCode.morseMap.contains))
-      throw new IllegalArgumentException("Input contains invalid characters.")
+      OutputHandler.printPrompt("Enter your message (English → Morse):")
+      val input = InputHandler.getUserInput()
+      println(s"[Debug] input: $input")
 
-    val encodedText  = Translator.encode(input)
-    OutputHandler.printEncoded(encodedText)
+      // TODO: Move input validation to Validation.scala
+      // Includes: check for empty input, unsupported characters and max length
+      if (!input.forall(MorseCode.morseMap.contains))
+        throw new IllegalArgumentException("Input contains invalid characters.")
 
+      val encodedText = Translator.encode(input)
+      OutputHandler.printEncoded(encodedText)
+
+      keepTranslating = askToContinue()
+    }
+
+  }
+}
+
+@tailrec
+private def askToContinue(): Boolean = {
+  OutputHandler.printPrompt("Do you want to translate another message? Y/N")
+  InputHandler.getUserInput() match {
+    case "Y" => true
+    case "N" =>
+      OutputHandler.printPrompt("Goodbye")
+      false
+    case _ =>
+      OutputHandler.printError("Invalid input.")
+      askToContinue()
+    // change to printError later and think recursively retry
   }
 }
