374d5f5538d8d65cc3135a1560bc196e9ebe36a8
edpau <edpau@me.com>
2025-07-16 17:04:09 +0100

    style: auto-format Validation file for consistency

--- src/main/scala/validator/Validation.scala
+++ src/main/scala/validator/Validation.scala
@@ -3,13 +3,13 @@ package validator
 import translator.MorseCode
 
 object Validation {
-  
+
   def validateEncodeInput(input: String): Either[String, String] = {
     if (input.isEmpty)
       Left("Input is empty.")
     else if (!input.forall(MorseCode.charToMorse.contains))
       Left("Input contains unsupported characters.")
-    else 
+    else
       Right(input)
   }
 
@@ -19,13 +19,13 @@ object Validation {
 
     val morseWords = input.split(" / ")
 
-    val allValid = morseWords.forall{ word =>
+    val allValid = morseWords.forall { word =>
       val letters = word.split(" ")
       letters.forall(MorseCode.morseToChar.contains)
     }
 
-    if(!allValid)
-     return Left("Input contains unsupported characters.")
+    if (!allValid)
+      return Left("Input contains unsupported characters.")
 
     Right(input)
   }
